bb06ea837cb3d3ced1f3e54c8a2735e95f66af32
Ghostcatlol <voidddeverdade@gmail.com>
2025-06-17 14:40:35 +0100

    Removed Avatar

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -163,10 +163,6 @@ export function Header() {
       </div>
 
       <Avatar className="size-28" aria-hidden="true">
-        <AvatarImage
-          alt={`${RESUME_DATA.name}'s profile picture`}
-          src={RESUME_DATA.avatarUrl}
-        />
         <AvatarFallback>{RESUME_DATA.initials}</AvatarFallback>
       </Avatar>
     </header>
